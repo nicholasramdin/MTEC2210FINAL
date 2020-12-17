6f18ebae157cc487e6107646eb79a6a97bb734d7
nicholasramdin <nicholasramdin@gmail.com>
2020-12-17 17:41:07 -0500

    final
    final iteration. fixed game over and enemy bullet

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    private Transform bullet;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bullet = GetComponent<Transform>();
+    }
+    private void FixedUpdate()
+    {
+        bullet.position += Vector3.up * speed;
+        if (bullet.position.y >= 10)
+            Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.tag == "Enemy")
+        {
+
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            PlayerScore.playerScore += 10;
+        }
+        else if (other.tag == "Base")
+            Destroy(gameObject);
+
     }
 }
+
